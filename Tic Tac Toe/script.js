33bc93ecdfd59cb3019ed88764c960bf58dfae94
Juan Pablo <juanpablomaine@gmail.com>
2023-11-29 19:00:15 -0300

    .

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -6,6 +6,7 @@ let gameBoard = [[],
 
 let totalMoves = 0;
 let currentPlayer = "X";
+let winner = false;
 
 const play = (column, row, squareClass) =>{
     if(column > 2 || column < 0 || row > 2 || row < 0 || gameBoard[column][row]!= undefined) console.log("imposible move");
@@ -25,42 +26,50 @@ const checkResult = () =>{
     for(let i = 0; i < 3; i++){
         if(gameBoard[i][0] === "X" && gameBoard[i][1] === "X" && gameBoard[i][2] === "X"){
             RESULT.textContent = "Result: Player 1 (X) wins";  //Check horizontal lines
+            winner = true;
             GAME_BOARD.style.pointerEvents = "none";
         } 
         else if((gameBoard[i][0] === "O" && gameBoard[i][1] === "O" && gameBoard[i][2] === "O")){
             RESULT.textContent = "Result: Player 2 (O) wins";  //Check horizontal lines
+            winner = true;
             GAME_BOARD.style.pointerEvents = "none";
         } 
         
         else if (gameBoard[0][i] === "X" && gameBoard[1][i] === "X" && gameBoard[2][i] === "X"){ //Check vertical lines
             RESULT.textContent = "Result: Player 1 (X) wins"; 
+            winner = true;
             GAME_BOARD.style.pointerEvents = "none";
         } 
         else if (gameBoard[0][i] === "O" && gameBoard[1][i] === "O" && gameBoard[2][i] === "O"){ //Check vertical lines
             RESULT.textContent = "Result: Player 2 (O) wins"; 
+            winner = true;
             GAME_BOARD.style.pointerEvents = "none";
         } 
     }
 
     if(gameBoard[0][0] === "X" && gameBoard[1][1] === "X" && gameBoard[2][2] === "X"){ //Check first cross line
         RESULT.textContent = "Result: Player 1 (X) wins"; 
+        winner = true;
         GAME_BOARD.style.pointerEvents = "none";
     } 
     else if(gameBoard[0][0] === "O" && gameBoard[1][1] === "O" && gameBoard[2][2] === "O"){ //Check first cross line
-        RESULT.textContent = "Result: Player 2 (O) wins"; 
+        RESULT.textContent = "Result: Player 2 (O) wins";
+        winner = true; 
         GAME_BOARD.style.pointerEvents = "none";
     } 
 
     else if(gameBoard[2][0] === "X" && gameBoard[1][1] === "X" && gameBoard[0][2] === "X"){ //Check second cross line
         RESULT.textContent = "Result: Player 1 (X) wins"; 
+        winner = true;
         GAME_BOARD.style.pointerEvents = "none";
     } 
     else if(gameBoard[2][0] === "O" && gameBoard[1][1] === "O" && gameBoard[0][2] === "O"){ //Check second cross line
         RESULT.textContent = "Result: Player 2 (O) wins"; 
+        winner = true;
         GAME_BOARD.style.pointerEvents = "none";
     } 
-    
-    if(totalMoves === 9){
+
+    if(totalMoves === 9 && winner === false){
         RESULT.textContent = "Result: Draw";
         GAME_BOARD.style.pointerEvents = "none";
     }
